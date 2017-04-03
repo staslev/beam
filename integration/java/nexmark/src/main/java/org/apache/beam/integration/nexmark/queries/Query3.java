9a71c530526bdea81ad40642eba0cf3c29a7557c
Etienne Chauchot <echauchot@gmail.com>
2017-04-03 15:18:04 +0200

    Query3: Use globalWindow Issue #7

--- integration/java/nexmark/src/main/java/org/apache/beam/integration/nexmark/queries/Query3.java
+++ integration/java/nexmark/src/main/java/org/apache/beam/integration/nexmark/queries/Query3.java
@@ -62,31 +62,123 @@ import org.slf4j.LoggerFactory;
  * </pre>
  *
  * <p>We'll implement this query to allow 'new auction' events to come before the 'new person'
- * events for the auction seller. Those auctions will be stored until the matching person is
- * seen. Then all subsequent auctions for a person will use the stored person record.
+ * events for the auction seller. Those auctions will be stored until the matching person is seen.
+ * Then all subsequent auctions for a person will use the stored person record.
  *
  * <p>A real system would use an external system to maintain the id-to-person association.
  */
 public class Query3 extends NexmarkQuery {
+
   private static final Logger LOG = LoggerFactory.getLogger(Query3.class);
-//  private static final StateContext GLOBAL_NAMESPACE = StateContexts.global();
-  private static final StateSpec<Object, ValueState<List<Auction>>> AUCTION_LIST_CODED_TAG =
-      StateSpecs.value(ListCoder.of(Auction.CODER));
-  private static final StateSpec<Object, ValueState<Person>> PERSON_CODED_TAG =
-      StateSpecs.value(Person.CODER);
+  private final JoinDoFn joinDoFn = new JoinDoFn();
+
+  public Query3(NexmarkConfiguration configuration) {
+    super(configuration, "Query3");
+  }
+
+  @Override
+  @Nullable
+  public Aggregator<Long, Long> getFatalCount() {
+    return joinDoFn.fatalCounter;
+  }
+
+  private PCollection<NameCityStateId> applyTyped(PCollection<Event> events) {
+
+    PCollection<KV<Long, Auction>> auctionsBySellerId =
+        events
+            // Only want the new auction events.
+            .apply(JUST_NEW_AUCTIONS)
+
+            // We only want auctions in category 10.
+            .apply(
+                name + ".InCategory",
+                Filter.by(
+                    new SerializableFunction<Auction, Boolean>() {
+
+                      @Override
+                      public Boolean apply(Auction auction) {
+                        return auction.category == 10;
+                      }
+                    }))
+
+            // Key auctions by their seller id.
+            .apply("AuctionBySeller", AUCTION_BY_SELLER);
+
+    PCollection<KV<Long, Person>> personsById =
+        events
+            // Only want the new people events.
+            .apply(JUST_NEW_PERSONS)
+
+            // We only want people in OR, ID, CA.
+            .apply(
+                name + ".InState",
+                Filter.by(
+                    new SerializableFunction<Person, Boolean>() {
+
+                      @Override
+                      public Boolean apply(Person person) {
+                        return person.state.equals("OR")
+                            || person.state.equals("ID")
+                            || person.state.equals("CA");
+                      }
+                    }))
+
+            // Key people by their id.
+            .apply("PersonById", PERSON_BY_ID);
+
+    return
+    // Join auctions and people.
+    // concatenate KeyedPCollections
+    KeyedPCollectionTuple.of(AUCTION_TAG, auctionsBySellerId)
+        .and(PERSON_TAG, personsById)
+        // group auctions and persons by personId
+        .apply(CoGroupByKey.<Long>create())
+        .apply(name + ".Join", ParDo.of(joinDoFn))
+
+        // Project what we want.
+        .apply(
+            name + ".Project",
+            ParDo.of(
+                new DoFn<KV<Auction, Person>, NameCityStateId>() {
+
+                  @ProcessElement
+                  public void processElement(ProcessContext c) {
+                    Auction auction = c.element().getKey();
+                    Person person = c.element().getValue();
+                    c.output(
+                        new NameCityStateId(person.name, person.city, person.state, auction.id));
+                  }
+                }));
+  }
+
+  @Override
+  protected PCollection<KnownSize> applyPrim(PCollection<Event> events) {
+    return NexmarkUtils.castToKnownSize(name, applyTyped(events));
+  }
 
   /**
-   * Join {@code auctions} and {@code people} by person id and emit their cross-product one pair
-   * at a time.
+   * Join {@code auctions} and {@code people} by person id and emit their cross-product one pair at
+   * a time.
    *
    * <p>We know a person may submit any number of auctions. Thus new person event must have the
    * person record stored in persistent state in order to match future auctions by that person.
    *
-   * <p>However we know that each auction is associated with at most one person, so only need
-   * to store auction records in persistent state until we have seen the corresponding person
-   * record. And of course may have already seen that record.
+   * <p>However we know that each auction is associated with at most one person, so only need to
+   * store auction records in persistent state until we have seen the corresponding person record.
+   * And of course may have already seen that record.
    */
   private static class JoinDoFn extends DoFn<KV<Long, CoGbkResult>, KV<Auction, Person>> {
+
+    private static final String AUCTIONS = "auctions";
+    private static final String PERSON = "person";
+
+    @StateId(PERSON)
+    private static final StateSpec<Object, ValueState<Person>> personSpec =
+        StateSpecs.value(Person.CODER);
+    public final Aggregator<Long, Long> fatalCounter = createAggregator("fatal", Sum.ofLongs());
+    @StateId(AUCTIONS)
+    private final StateSpec<Object, ValueState<List<Auction>>> auctionsSpec =
+        StateSpecs.value(ListCoder.of(Auction.CODER));
     private final Aggregator<Long, Long> newAuctionCounter =
         createAggregator("newAuction", Sum.ofLongs());
     private final Aggregator<Long, Long> newPersonCounter =
@@ -97,20 +189,20 @@ public class Query3 extends NexmarkQuery {
         createAggregator("newOldOutput", Sum.ofLongs());
     private final Aggregator<Long, Long> oldNewOutputCounter =
         createAggregator("oldNewOutput", Sum.ofLongs());
-    public final Aggregator<Long, Long> fatalCounter = createAggregator("fatal", Sum.ofLongs());
 
     @ProcessElement
-    public void processElement(ProcessContext c) throws IOException {
-      //TODO: This is using the internal state API. Rework to use the
-      //TODO Ismael this is broken for not access to state
+    public void processElement(
+        ProcessContext c,
+        @StateId(PERSON) ValueState<Person> personState,
+        @StateId(AUCTIONS) ValueState<List<Auction>> auctionsState)
+        throws IOException {
       // We would *almost* implement this by  rewindowing into the global window and
       // running a combiner over the result. The combiner's accumulator would be the
       // state we use below. However, combiners cannot emit intermediate results, thus
       // we need to wait for the pending ReduceFn API.
-//      StateInternals<?> stateInternals = c.windowingInternals().stateInternals();
-//      ValueState<Person> personState = stateInternals.state(GLOBAL_NAMESPACE, PERSON_CODED_TAG);
-//      Person existingPerson = personState.read();
-      Person existingPerson = null;
+
+      Person existingPerson = personState.read();
+
       if (existingPerson != null) {
         // We've already seen the new person event for this person id.
         // We can join with any new auctions on-the-fly without needing any
@@ -123,8 +215,6 @@ public class Query3 extends NexmarkQuery {
         return;
       }
 
-//      ValueState<List<Auction>> auctionsState =
-//          stateInternals.state(GLOBAL_NAMESPACE, AUCTION_LIST_CODED_TAG);
       Person theNewPerson = null;
       for (Person newPerson : c.element().getValue().getAll(PERSON_TAG)) {
         if (theNewPerson == null) {
@@ -141,13 +231,13 @@ public class Query3 extends NexmarkQuery {
         newPersonCounter.addValue(1L);
         // We've now seen the person for this person id so can flush any
         // pending auctions for the same seller id.
-        List<Auction> pendingAuctions = null; //auctionsState.read();
+        List<Auction> pendingAuctions = auctionsState.read();
         if (pendingAuctions != null) {
           for (Auction pendingAuction : pendingAuctions) {
             oldNewOutputCounter.addValue(1L);
             c.output(KV.of(pendingAuction, newPerson));
           }
-//          auctionsState.clear();
+          auctionsState.clear();
         }
         // Also deal with any new auctions.
         for (Auction newAuction : c.element().getValue().getAll(AUCTION_TAG)) {
@@ -156,8 +246,7 @@ public class Query3 extends NexmarkQuery {
           c.output(KV.of(newAuction, newPerson));
         }
         // Remember this person for any future auctions.
-
-//        personState.write(newPerson);
+        personState.write(newPerson);
       }
       if (theNewPerson != null) {
         return;
@@ -165,7 +254,7 @@ public class Query3 extends NexmarkQuery {
 
       // We'll need to remember the auctions until we see the corresponding
       // new person event.
-      List<Auction> pendingAuctions = null; //auctionsState.read();
+      List<Auction> pendingAuctions = auctionsState.read();
       if (pendingAuctions == null) {
         pendingAuctions = new ArrayList<>();
       }
@@ -173,84 +262,7 @@ public class Query3 extends NexmarkQuery {
         newAuctionCounter.addValue(1L);
         pendingAuctions.add(newAuction);
       }
-//      auctionsState.write(pendingAuctions);
+      auctionsState.write(pendingAuctions);
     }
-  }
-
-  private final JoinDoFn joinDoFn = new JoinDoFn();
-
-  public Query3(NexmarkConfiguration configuration) {
-    super(configuration, "Query3");
-  }
-
-  @Override
-  @Nullable
-  public Aggregator<Long, Long> getFatalCount() {
-    return joinDoFn.fatalCounter;
-  }
-
-  private PCollection<NameCityStateId> applyTyped(PCollection<Event> events) {
-    // Batch into incremental results windows.
-    events = events.apply(
-        Window.<Event>into(FixedWindows.of(Duration.standardSeconds(configuration.windowSizeSec))));
-
-    PCollection<KV<Long, Auction>> auctionsBySellerId =
-        events
-            // Only want the new auction events.
-            .apply(JUST_NEW_AUCTIONS)
-
-            // We only want auctions in category 10.
-            .apply(name + ".InCategory", Filter.by(new SerializableFunction<Auction, Boolean>() {
-              @Override
-              public Boolean apply(Auction auction) {
-                return auction.category == 10;
-              }
-            }))
-
-            // Key auctions by their seller id.
-            .apply("AuctionBySeller", AUCTION_BY_SELLER);
-
-    PCollection<KV<Long, Person>> personsById =
-        events
-            // Only want the new people events.
-            .apply(JUST_NEW_PERSONS)
-
-            // We only want people in OR, ID, CA.
-            .apply(name + ".InState", Filter.by(new SerializableFunction<Person, Boolean>() {
-              @Override
-              public Boolean apply(Person person) {
-                return person.state.equals("OR") || person.state.equals("ID")
-                    || person.state.equals("CA");
-              }
-            }))
-
-            // Key people by their id.
-            .apply("PersonById", PERSON_BY_ID);
-
-    return
-      // Join auctions and people.
-        // concatenate KeyedPCollections
-      KeyedPCollectionTuple.of(AUCTION_TAG, auctionsBySellerId)
-            .and(PERSON_TAG, personsById)
-        // group auctions and persons by personId
-        .apply(CoGroupByKey.<Long>create())
-            .apply(name + ".Join", ParDo.of(joinDoFn))
-
-            // Project what we want.
-            .apply(name + ".Project",
-                ParDo.of(new DoFn<KV<Auction, Person>, NameCityStateId>() {
-                      @ProcessElement
-                      public void processElement(ProcessContext c) {
-                        Auction auction = c.element().getKey();
-                        Person person = c.element().getValue();
-                        c.output(new NameCityStateId(
-                            person.name, person.city, person.state, auction.id));
-                      }
-                    }));
-  }
-
-  @Override
-  protected PCollection<KnownSize> applyPrim(PCollection<Event> events) {
-    return NexmarkUtils.castToKnownSize(name, applyTyped(events));
   }
 }
